0cfa4df3c8bd4d1fde5276c16af335a6dcb5dd2f
Lin L Du <novaline@qq.com>
2018-04-24 20:37:04 +0800

    client端分页查询初步完成

--- client/src/containers/topics/index.js
+++ client/src/containers/topics/index.js
@@ -1,23 +1,57 @@
 import React from 'react';
-import { Query, graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 import * as Q from './gql';
 
 class Topics extends React.Component {
+  constructor() {
+    super();
+    this.page = 1;
+    this.limit = 2;
+  }
+  onLoadMore(fetchMore) {
+    const page = this.page + 1;
+    return fetchMore({
+      variables: {
+        qs: {
+          page,
+          limit: this.limit
+        }
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev;
+        return Object.assign({}, prev, {
+          topics: [...prev.topics, ...fetchMoreResult.topics]
+        });
+      }
+    });
+  }
   render() {
     return (
       <div>
         <h2>Topics</h2>
-        <Query query={Q.getHomeTopics}>
-          {({ loading, error, data: { topics } }) => {
+        <Query
+          query={Q.getHomeTopics}
+          variables={{
+            qs: {
+              page: this.page,
+              limit: this.limit
+            }
+          }}>
+          {({ loading, error, data: { topics }, fetchMore }) => {
             if (loading) return <p>loading...</p>;
             if (error) return <p>error</p>;
             return (
-              <ul>
-                {topics.map(topic => {
-                  return <li key={topic.id}>{topic.title}</li>;
-                })}
-              </ul>
+              <div>
+                <ul>
+                  {topics.map(topic => {
+                    return <li key={topic.id}>{topic.title}</li>;
+                  })}
+                </ul>
+                <button type="button" onClick={() => this.onLoadMore(fetchMore)}>
+                  加载更多
+                </button>
+              </div>
             );
           }}
         </Query>
@@ -26,10 +60,4 @@ class Topics extends React.Component {
   }
 }
 
-export default graphql(Q.getHomeTopics, {
-  options: props => {
-    return {
-      variables: {}
-    };
-  }
-})(Topics);
+export default Topics;
