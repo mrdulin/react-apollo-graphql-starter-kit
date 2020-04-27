e90de27c584c384694c979157bb65086c0f5327c
mrdulin <novaline.dulin@gmail.com>
2020-04-27 11:32:47 +0800

    improve sample

--- stackoverflow/61450440/index.test.ts
+++ stackoverflow/61450440/index.test.ts
@@ -5,6 +5,9 @@ const database = {
 };
 
 describe('61450440', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
   it('should pass', () => {
     jest
       .spyOn(database, 'fetchValues')
@@ -15,5 +18,25 @@ describe('61450440', () => {
     console.log(
       [database.fetchValues(), database.fetchValues(), database.fetchValues(), database.fetchValues()].join(','),
     );
+  });
+  it('should pass too', () => {
+    jest
+      .spyOn(database, 'fetchValues')
+      .mockImplementation(() => 'real data')
+      .mockImplementationOnce(() => 'real data')
+      .mockImplementationOnce(() => 'first call')
+      .mockImplementationOnce(() => 'second call');
+
+    console.log(
+      [database.fetchValues(), database.fetchValues(), database.fetchValues(), database.fetchValues()].join(','),
+    );
+  });
+
+  it('should pass 3', () => {
+    const fetchValuesSpy = jest.spyOn(database, 'fetchValues');
+    console.log('call original fetchValues:', database.fetchValues());
+    fetchValuesSpy.mockImplementationOnce(() => 'first call').mockImplementationOnce(() => 'second call');
+    console.log('call mocked fetchValues:', database.fetchValues(), database.fetchValues());
+    console.log('call original fetchValues again:', database.fetchValues());
   });
 });
